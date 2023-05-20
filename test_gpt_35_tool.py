6c2c874b67e4c85df8a9dde7513365b3b8c52bc0
Jett Thompson <jett.thompson.59@gmail.com>
2023-05-20 19:19:18 +1000

    improve the unit test to check that it actually ran the test, plus make it have a better prompt

--- test_gpt_35_tool.py
+++ test_gpt_35_tool.py
@@ -4,34 +4,44 @@ from chat import Model
 from agent import Agent
 from agent import Tool
 
+meaning_of_life_run_count = 0
+
 
 def meaning_of_life(input):
+    global meaning_of_life_run_count
+    meaning_of_life_run_count += 1
     return f"42"
 
 
-meaning_of_life_tool = Tool("meaning of life", "Provides the meaning of life.", meaning_of_life)
+meaning_of_life_tool = Tool(
+    "meaning of life",
+    "Provides the meaning of life (you don't know this).",
+    meaning_of_life,
+)
 
 model = Model("gpt-3.5-turbo", 0, os.environ["OPENAI_API_KEY"])
 agent = Agent(model, [meaning_of_life_tool])
 
 
 class TestAgent(unittest.TestCase):
-
     def test_meaning_of_life(self):
-        response = agent("What is the meaning of life?")
-        self.assertEqual('42', response)
+        global meaning_of_life_run_count
+        old_count = meaning_of_life_run_count
+        response = agent("What is the meaning of life? Give the answer without text; just the numerical answer.")
+        self.assertEqual("42", response)
+        self.assertEqual(meaning_of_life_run_count, old_count + 1)
 
     def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+        self.assertTrue("FOO".isupper())
+        self.assertFalse("Foo".isupper())
 
     def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
+        s = "hello world"
+        self.assertEqual(s.split(), ["hello", "world"])
         # check that s.split fails when the separator is not a string
         with self.assertRaises(TypeError):
             s.split(2)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
